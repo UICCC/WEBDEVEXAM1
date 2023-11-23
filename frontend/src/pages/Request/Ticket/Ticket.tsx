c0338858c79b3a74ad41b46ec1a91e1acd8ef547
UICCC <142647371+UICCC@users.noreply.github.com>
2023-11-24 07:39:17 +0800

    Update Ticket.tsx

--- frontend/src/pages/Request/Ticket/Ticket.tsx
+++ frontend/src/pages/Request/Ticket/Ticket.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Ticket.css'
+import { InputText } from 'primereact/inputtext';
+        
 
 
 function Ticket() {
+    const [valueequipment, setValueticket] = useState('');
+    const [valuepurpose, setValuepurpose] = useState('');
+    const [valuedate, setValuedate] = useState('');
 
     return(
         <>
@@ -17,6 +22,28 @@ function Ticket() {
         <div className='thanks-subtile'>
             <p> We have recieved you application and will get back to you soon <b>God bless! </b></p>
         </div>
+
+        <div className='equipmentborrowed'>
+            <p>Equipmentborrowed: </p>
+            </div>
+            <div className="equipmentticketbox">
+            <InputText value={valueequipment} onChange={(e) => setValueticket(e.target.value)} />
+        </div>
+            <div className='purposeticket'>
+            <p> Purpose: </p>
+            </div>
+            <div className="purposeticketbox">
+            <InputText value={valuepurpose} onChange={(e) => setValuepurpose(e.target.value)} />
+        </div>
+
+            <div className='datetimeticket'>
+            <p> Date and time: </p>
+            </div>
+            <div className="dateticketbox">
+            <InputText value={valuedate} onChange={(e) => setValuedate(e.target.value)} />
+        </div>
+
+        
 
 
         </div>
