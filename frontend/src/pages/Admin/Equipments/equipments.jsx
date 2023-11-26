168bc138270723dbbcd2b63e77ab0643a6d95543
UICCC <142647371+UICCC@users.noreply.github.com>
2023-11-26 11:16:17 +0800

    asdadas

---
+++ frontend/src/pages/Admin/Equipments/equipments.jsx
@@ -0,0 +1,24 @@
+import './equipments.css'
+
+
+
+
+
+
+function Equipments() {
+
+   
+    
+
+  
+      
+    return(
+        <>
+       
+       asds
+    
+         
+        </>
+    )
+}
+export default Equipments
