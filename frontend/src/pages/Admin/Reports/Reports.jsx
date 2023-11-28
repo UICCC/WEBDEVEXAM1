5aacce668642fdcaa3ba938c7397f3206aaf9cd9
UICCC <142647371+UICCC@users.noreply.github.com>
2023-11-29 07:44:32 +0800

    gggh

--- frontend/src/pages/Admin/Reports/Reports.jsx
+++ frontend/src/pages/Admin/Reports/Reports.jsx
@@ -15,9 +15,62 @@ function Report() {
     return (
         <>
 
-        
-
-       asdasd
+<h1 className='feebacktitle'>Feedback</h1>
+<table class="custom-table">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Course</th>
+            <th>Equipment</th>
+            <th>Rating</th>
+            <th>Comments</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Luke</td>
+            <td>BSIT-4a</td>
+            <td>Audio Cord</td>
+            <td>5</td>
+            <td>Worked fine</td>
+        </tr>
+        <tr>
+            <td>Emma</td>
+            <td>Engineering-3b</td>
+            <td>Projector</td>
+            <td>4</td>
+            <td>Good quality</td>
+        </tr>
+        <tr>
+            <td>Michael</td>
+            <td>Business-2c</td>
+            <td>Laptop</td>
+            <td>3</td>
+            <td>Slow performance</td>
+        </tr>
+        <tr>
+            <td>Sophia</td>
+            <td>Medicine-1d</td>
+            <td>Microscope</td>
+            <td>5</td>
+            <td>Excellent clarity</td>
+        </tr>
+        <tr>
+            <td>William</td>
+            <td>Arts-5e</td>
+            <td>Easel</td>
+            <td>4</td>
+            <td>Sturdy and reliable</td>
+        </tr>
+        <tr>
+            <td>Ava</td>
+            <td>Science-2f</td>
+            <td>Lab Coat</td>
+            <td>4</td>
+            <td>Comfortable and durable</td>
+        </tr>
+    </tbody>
+</table>
       
           
             
