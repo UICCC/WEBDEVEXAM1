dcad0cfe5ee7e194e4f153b2b55d6935d66351a6
UICCC <142647371+UICCC@users.noreply.github.com>
2024-05-05 20:09:26 +0800

    next commit

--- frontend/src/pages/Personnel/Personnel.jsx
+++ frontend/src/pages/Personnel/Personnel.jsx
@@ -23,40 +23,13 @@ function Personnel() {
             icon: 'pi pi-home'
         },
         {
-            label: 'Features',
+            label: 'Overview',
             icon: 'pi pi-star'
         },
         {
-            label: 'Projects',
+            label: 'Equipments',
             icon: 'pi pi-search',
-            items: [
-                {
-                    label: 'Components',
-                    icon: 'pi pi-bolt'
-                },
-                {
-                    label: 'Blocks',
-                    icon: 'pi pi-server'
-                },
-                {
-                    label: 'UI Kit',
-                    icon: 'pi pi-pencil'
-                },
-                {
-                    label: 'Templates',
-                    icon: 'pi pi-palette',
-                    items: [
-                        {
-                            label: 'Apollo',
-                            icon: 'pi pi-palette'
-                        },
-                        {
-                            label: 'Ultima',
-                            icon: 'pi pi-palette'
-                        }
-                    ]
-                }
-            ]
+           
         },
         {
             label: 'Contact',
